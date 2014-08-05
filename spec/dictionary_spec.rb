3164755838880b34bf75abf2315f5c71eef5e107
Derfler <rmderfler@gmail.com>
2014-08-05 15:00:17 -0700

    it returns the definition

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -3,15 +3,21 @@ require 'term'
 
 describe 'Term' do
 
-it 'initializes a new term' do
-  new_term = Term.new("new", "shiny")
-  expect(new_term).to be_an_instance_of Term
-end
+  it 'initializes a new term' do
+    new_term = Term.new("new", "shiny")
+    expect(new_term).to be_an_instance_of Term
+  end
 
-it "returns a term's 'word'" do
-  new_term = Term.new("new", "shiny")
-  expect(new_term.word).to eq "new"
-end
+  it "returns a term's 'word'" do
+    new_term = Term.new("new", "shiny")
+    expect(new_term.word).to eq "new"
+  end
+
+  it "returns a term's definition" do
+    new_term = Term.new("new", "shiny")
+    expect(new_term.definition).to eq "shiny"
+  end
+
 
 
 end
