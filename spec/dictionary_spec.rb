36c857bf4091b8c4d2f2190afc82b0837fbb30e4
Derfler <rmderfler@gmail.com>
2014-08-05 14:58:19 -0700

    it returns a term'sword

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,7 +4,14 @@ require 'term'
 describe 'Term' do
 
 it 'initializes a new term' do
-    new_term = Term.new("new", "shiny")
-    expect(new_term).to be_an_instance_of Term
-  end
+  new_term = Term.new("new", "shiny")
+  expect(new_term).to be_an_instance_of Term
+end
+
+it "returns a term's 'word'" do
+  new_term = Term.new("new", "shiny")
+  expect(new_term.word).to eq "new"
+end
+
+
 end
